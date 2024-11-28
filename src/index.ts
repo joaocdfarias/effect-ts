d529e88604fcbfb0d144d40ebb5b497411eeae1c
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-11-28 01:10:06 -0300

    feat: uses Effect to handle errors

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Effect } from "effect";
+
+interface FetchError {
+  readonly _tag: "FetchError";
+}
+
+interface JsonError {
+  readonly _tag: "JsonError";
+}
+
+const fetchRequest = Effect.tryPromise({
+  try: () => fetch("https://pokeapi.co/api/v22/pokemon/garchomp/"),
+  catch: (): FetchError => ({ _tag: "FetchError" }),
+});
+
+const jsonResponse = (response: Response) =>
+  Effect.tryPromise({
+    try: () => response.json(),
+    catch: (): JsonError => ({ _tag: "JsonError" }),
+  });
+
+const main = fetchRequest.pipe(
+  Effect.flatMap(jsonResponse),
+  Effect.catchTags({
+    FetchError: () => Effect.succeed("Fetch error"),
+    JsonError: () => Effect.succeed("Json error"),
+  })
+);
+
+Effect.runPromise(main).then(console.log);
